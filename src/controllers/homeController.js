9dbd49fadac656b75c1ae19bec0c1de430dd0e6b
ThaiLe <thailevlfyb@gmail.com>
2023-08-13 08:48:07 +0700

    Add try-catch block to controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,42 +3,95 @@ import * as CrudService from "../services/CrudService.js";
 const bcrypt = require("bcrypt");
 
 const getHomePage = async (req, res) => {
-    const data = await db.Users.findAll();
+    try {
+        const data = await db.Users.findAll();
 
-    return res.render("homePage.ejs", { data: JSON.stringify(data) });
+        return res.render("homePage.ejs", { data: JSON.stringify(data) });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const getAboutPage = (req, res) => {
-    return res.render("aboutPage.ejs");
+    try {
+        return res.render("aboutPage.ejs");
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const getCRUD = (req, res) => {
-    return res.render("formCreateUser.ejs");
+    try {
+        return res.render("formCreateUser.ejs");
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const postCRUD = async (req, res) => {
-    const result = await CrudService.createUser(req.body);
-    return res.redirect("/list-user");
+    try {
+        const result = await CrudService.createUser(req.body);
+        return res.redirect("/list-user");
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const updateCRUD = async (req, res) => {
-    const result = await CrudService.updateUser(req.params.userId, req.body);
-    return res.redirect("/list-user");
+    try {
+        const result = await CrudService.updateUser(
+            req.params.userId,
+            req.body
+        );
+        return res.redirect("/list-user");
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const deleteCRUD = async (req, res) => {
-    const result = await CrudService.deleteUser(req.params);
-    return res.redirect("/list-user");
+    try {
+        const result = await CrudService.deleteUser(req.params);
+        return res.redirect("/list-user");
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const listUserPage = async (req, res) => {
-    const users = await db.Users.findAll();
-    return res.render("listUser.ejs", { users });
+    try {
+        const users = await db.Users.findAll();
+        return res.render("listUser.ejs", { users });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const updateUserPage = async (req, res) => {
-    const user = await db.Users.findOne({ where: { id: req.params.userId } });
-    return res.render("formEditUser.ejs", { user });
+    try {
+        const user = await db.Users.findOne({
+            where: { id: req.params.userId },
+        });
+        return res.render("formEditUser.ejs", { user });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 export {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,58 +1,94 @@
 import * as userService from "../services/userService";
 
 const handleLoginUser = async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    // console.log(req.body);
-    if (!email || !password) {
-        return res.status(500).json({
-            errCode: 1,
-            message: "Input is empty",
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        // console.log(req.body);
+        if (!email || !password) {
+            return res.status(500).json({
+                errCode: 1,
+                message: "Input is empty",
+            });
+        }
+
+        const userData = await userService.handleLogin(email, password);
+
+        return res.status(200).json({
+            userData,
         });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
     }
-
-    const userData = await userService.handleLogin(email, password);
-
-    return res.status(200).json({
-        userData,
-    });
 };
 
 const getUsers = async (req, res) => {
-    const users = await userService.getUsers(req.query.id);
+    try {
+        const users = await userService.getUsers(req.query.id);
 
-    return res.status(200).json({
-        users,
-    });
+        return res.status(200).json({
+            users,
+        });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const createUser = async (req, res) => {
-    const data = await userService.createUser(req.body);
-    return res.status(200).json({
-        data,
-    });
+    try {
+        const data = await userService.createUser(req.body);
+        return res.status(200).json({
+            data,
+        });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const updateUser = async (req, res) => {
-    const data = await userService.updateUser(req.body);
+    try {
+        const data = await userService.updateUser(req.body);
 
-    return res.status(200).json({
-        data,
-    });
+        return res.status(200).json({
+            data,
+        });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const deleteUser = async (req, res) => {
-    const data = await userService.deleteUser(req.body.id);
+    try {
+        const data = await userService.deleteUser(req.body.id);
 
-    return res.status(200).json({
-        data,
-    });
+        return res.status(200).json({
+            data,
+        });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 const getAllCode = async (req, res) => {
-    const data = await userService.getAllCode(req.query.type);
+    try {
+        const data = await userService.getAllCode(req.query.type);
 
-    return res.status(200).json({ data });
+        return res.status(200).json({ data });
+    } catch (e) {
+        return res
+            .status(200)
+            .json({ errCode: -1, message: "Get error from sever" });
+    }
 };
 
 export {
